307f8b05059252547b5ac97f2d22259a8d0fe35a
Natanalpe <natan.altomar14@gmail.com>
2022-01-17 00:14:42 -0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+import PageFilm from './src/screens/Filme/index'
+
+export default function App() {
+  return (
+      <PageFilm />
+  );
+}
